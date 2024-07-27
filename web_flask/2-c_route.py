d29616b4b8210ad649fc5ddc7b1d1bca47cbb72e
basma ebraheim <basmaebraheim@gmail.com>
2024-07-28 00:16:04 +0300

    c is function

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-starts Flask web application
+starts a Flask web application
 """
 
 from flask import Flask
-from markupsafe import escape
-
 app = Flask(__name__)
 
 
@@ -20,10 +18,10 @@ def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
-@app.route("/c/<text>", strict_slashes=False)
-def cIsFunc(text):
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
     """displays c followed by text"""
-    return f"C {escape(text.replace('_', ' '))}"
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
