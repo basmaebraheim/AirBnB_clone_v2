8ada177626ae8d0d135fbe23cff3029445393a23
basma ebraheim <basmaebraheim@gmail.com>
2024-07-27 23:52:49 +0300

    update routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,22 @@
-from flask import Flask
+#!/usr/bin/python3
+"""
+starts Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
